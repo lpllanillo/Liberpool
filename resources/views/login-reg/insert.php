88105d242e815623c2ae4aaddf96745ba496bfe7
lpllanillo <lebronjames409@gmail.com>
2018-08-05 17:38:45 +0800

    Initial commit

---
+++ resources/views/login-reg/insert.php
@@ -0,0 +1,23 @@
+<?php
+
+$con = mysqli_connect('localhost','root','');
+
+if(!$con){
+	echo 'Not Connected To Server';
+}
+
+$email = $_POST['remail'];
+$pass = $_POST['rpass'];
+$confPass = $_POST['rconfpass'];
+
+$sql = "INSERT INTO registration (email, password, confPass) VALUES ('$email', '$pass', '$confPass')";
+
+if(!mysqli_query($con,$sql)){
+	echo 'Not Inserted';
+}else{
+	echo 'Data Inserted';
+}
+
+header("refresh:1; url=login.php");
+
+?>
